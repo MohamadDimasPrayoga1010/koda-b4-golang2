78bee5a881c091fee10f9360ce7d5797e14beaa1
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-21 17:22:31 +0700

    feat: fix call dialing

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main(){
 	inputScore = append(inputScore, 88)
 	inputScore = append(inputScore, scores[3:]...)
 
-	// for x := range len(inputScore){
-	// 	fmt.Println(inputScore[x])
-	// }
+	for x := range len(inputScore){
+		fmt.Println(inputScore[x])
+	}
 
-	fmt.Println(inputScore)
+	
 }
